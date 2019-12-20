1891cebb7edca96627ff2bdb084b2dcdf56e1eb9
Paresh98000 <paresh98000@hotmail.com>
2019-12-20 19:59:14 +0530

    Updated RecyclerView Design border.xml and Home fragment

--- app/src/main/java/spksolutions/appointmentmaster/ui/home/HomeFragment.java
+++ app/src/main/java/spksolutions/appointmentmaster/ui/home/HomeFragment.java
@@ -11,17 +11,35 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import spksolutions.appointmentmaster.R;
+import spksolutions.appointmentmaster.adapter.Recycle_Home;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    private RecyclerView mR_View;
+    private RecyclerView.Adapter mR_Adapter;
+    private RecyclerView.LayoutManager mR_LayoutManager;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mR_View = root.findViewById(R.id.recyclerview_home);
+        mR_View.setHasFixedSize(true);
+
+        mR_LayoutManager = new LinearLayoutManager(getActivity());
+        mR_View.setLayoutManager(mR_LayoutManager);
+
+        Recycle_Home adapter = new Recycle_Home(new String[]{"Hellow","Paresh"},new String[]{"Hi...","Desc"},new String[]{"Hi...","Add"},new String[]{"Hi...",""},new int[]{R.drawable.appointment_1,R.drawable.appointment});
+        mR_View.setAdapter(adapter);
+
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -31,4 +49,6 @@ public class HomeFragment extends Fragment {
         });
         return root;
     }
+
+
 }
